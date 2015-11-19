5fe4ef1f64faba407b7053bb87898c8ed60e2f99
Mark Plagge <plaggm@rpi.edu>
2015-11-19 17:56:50 -0500

    Message Definition Code

---
+++ MessDef.py
@@ -0,0 +1,8 @@
+
+class NetMess(object):
+
+    def __init__(self,type="INVALID",m=0,accNum=1,accVal="Calendar goes here"):
+        self.type = type
+        self.m = m
+        self.accNum = accNum
+        self.accVal = accVal
