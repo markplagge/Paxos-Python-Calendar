ee13f48d83777046c3c41abedee21df4925819b1
Mark Plagge <plaggm@rpi.edu>
2015-11-23 15:23:46 -0500

    errr
    Server testing - shows that threads and async are having issues.

---
+++ Paxos/Acceptor.py
@@ -0,0 +1,27 @@
+﻿import threading
+import queue
+import leader.Leader
+import paxosObjs
+
+#Global Acceptor Queue:
+
+acceptorIn = queue.Queue()
+acceptorOut = queue.Queue()
+
+class Acceptor(threading.Thread):
+    """represents an accecptor in Paxos. Is threaded."""
+    def __init__(self, daemon = True, outQ = queue.Queue(), inQ = queue.Queue(), ldr = leader.Leader()):
+        self.daemon = daemon
+        self.outQ = outQ
+        self.inQ = inQ
+
+        self.ldr = ldr
+
+        return super().__init__()
+    
+    def run(self):
+        while(True):
+            if acceptorIn.qsize() > 0:
+                dest = self.ldr.clIP
+
+
--- simplenetwork/UDPio.py
+++ simplenetwork/UDPio.py
@@ -1,8 +1,12 @@
-from simplenetwork import serverData
+﻿from simplenetwork import serverData
 from simplenetwork.serverData import udpPort,udpDests
 import socketserver
 import socket
 import simplenetwork
+import threading
+import sys
+import random
+import time
 sq = serverData.mainServerQueue
 
 running = True
@@ -15,25 +19,70 @@ class udpDataHanlder(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request[0].strip()
         socket = self.request[1]
-        sq.outUDP.put(data.format())
+        if isinstance(data, tuple):
+            data = data[0]
+        sq.inUDP.put(data.decode())
         print("Got data: " + data.decode())
 
 class thUDPHandler(socketserver.ThreadingMixIn,socketserver.UDPServer):
     pass
 
+sndrRun = True
 def udpSendData():
-    while running:
+    udest = serverData.udpDests
+    port = serverData.udpPort
+    while sndrRun:
         if sq.outUDP.qsize() > 0:
             data = sq.outUDP.get(block=True)
-            for host in udpDests:
-
-                #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            if isinstance(data, tuple):
+                message = data[0]
+                destIP =data[1]
+                
                 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock.sendto(bytes(message.encode()),(destIP,port))
+            else:                      
+                for host in udest:
 
-                sock.sendto(bytes(data.encode()), host)
+                    #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+                    sock.sendto(bytes(data.encode()), (host,port))
+        time.sleep(2)
 def udpRun():
     UDPSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     UDPSock.bind(("",serverData.udpPort))
     while True:
         data,addr = UDPSock.recvfrom(5098)
         print(data.strip())
+
+def runUDP():
+    print("In Main")
+    host, port = "127.0.0.1",serverData.udpPort
+    udpserver = thUDPHandler((host,port),udpDataHanlder)
+    udp_thread = threading.Thread(target=udpserver.serve_forever)
+    udp_thread.daemon = True
+    udp_thread.start()
+    sender = threading.Thread(target=udpSendData)
+    sender.daemon = True
+    sender.start()
+    
+    return udpserver,sender 
+def testUDP():
+    
+    print("In Main")
+    host, port = "127.0.0.1",serverData.udpPort
+    udpserver = thUDPHandler((host,port),udpDataHanlder)
+    udp_thread = threading.Thread(target=udpserver.serve_forever)
+    udp_thread.daemon = True
+    udp_thread.start()
+    sender = threading.Thread(target=udpSendData)
+    sender.daemon = True
+    sender.start()
+    
+    return udpserver,sender 
+    
+def main():
+    testUDP()
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
