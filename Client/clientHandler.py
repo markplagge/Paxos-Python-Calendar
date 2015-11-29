4d9ea80cc29250dbc6351782eecb1d7ae6b56f73
Mark Plagge <plaggm@rpi.edu>
2015-11-29 16:19:12 -0500

    skeletion for acceptor
    acceptor skell

---
+++ Client/clientHandler.py
@@ -0,0 +1,6 @@
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from SocketServer import ThreadingMixIn
+import threading
+import argparse
+import re
+import cgi
--- Paxos/Acceptor.py
+++ Paxos/Acceptor.py
@@ -17,11 +17,25 @@ class Acceptor(threading.Thread):
 
         self.ldr = ldr
 
+        self.latestPromise = 0
+        self.latestPromiseData = None
+
         return super().__init__()
     
+    def newPromise(pNum, Pval):
+        """Runs when the incomming suggested value is bigger than the current promised value"""
+        pass
+    def extractMessage(msg):
+        return msg[0],msg[1] 
     def run(self):
         while(True):
             if acceptorIn.qsize() > 0:
                 dest = self.ldr.clIP
+                prID,dat = acceptorIn.get()
+                if self.latestPromise < prID:
+                    self.newPromise(prID,dat)
+                else:
+                    pass
+
 
 
