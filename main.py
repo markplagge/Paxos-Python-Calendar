8fd01dc57287445f574bc961f1d0de024f96a1d2
Neil McGlohon <nmcglohon@gmail.com>
2015-12-05 21:56:49 -0500

    updated arguments

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import client.Client
-
+import sys
 
 if __name__ == '__main__':
     print("Starting")
-    theClient = client.Client.Client(1)
+
+    thisPID = sys.argv[1]
+    nodes = sys.argv[2]
+
+    theClient = client.Client.Client(pID=thisPID, N=nodes, hostFile='./hosts.info')
 
     theClient.execClient()
