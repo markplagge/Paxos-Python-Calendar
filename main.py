fdd4a0845744b5ac3c70f64361d080d33d777e26
Neil McGlohon <nmcglohon@gmail.com>
2015-12-05 22:03:42 -0500

    fixing type of args

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 if __name__ == '__main__':
     print("Starting")
 
-    thisPID = sys.argv[1]
-    nodes = sys.argv[2]
+    thisPID = int(sys.argv[1])
+    nodes = int(sys.argv[2])
 
     theClient = client.Client.Client(pID=thisPID, N=nodes, hostFile='./hosts.info')
 
